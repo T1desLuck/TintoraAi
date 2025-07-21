c4cb2635baeba8331d6469c1c7c2614aaf3a015f
T1desLuck <tidesluck@icloud.com>
2025-07-21 16:20:38 +0500

    Update postprocess.py

--- src/model/postprocess.py
+++ src/model/postprocess.py
@@ -3,7 +3,8 @@ import numpy as np
 
 
 def postprocess_image(output_tensor, original_size, saturation=1.0):
-    """Конвертирует тензор в изображение, обрезает padding, применяет насыщенность."""
+    """Конвертирует тензор в изображение, обрезает padding,
+    применяет насыщенность."""
     output = output_tensor.squeeze().permute(1, 2, 0).cpu().numpy()
     output = output * saturation  # Настройка насыщенности
     output = np.clip(output * 255, 0, 255).astype(np.uint8)
