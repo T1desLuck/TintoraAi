15937907abb1757f99a7577051dba363807a840f
T1desLuck <tidesluck@icloud.com>
2025-07-22 10:06:44 +0500

    Update tintora_ai.py

--- src/model/tintora_ai.py
+++ src/model/tintora_ai.py
@@ -2,61 +2,79 @@ import torch
 import torch.nn as nn
 
 
-class TintoraAI(nn.Module):
-    def __init__(self, in_channels=1, out_channels=3):
-        super(TintoraAI, self).__init__()
-
-        def conv_block(in_ch, out_ch):
-            return nn.Sequential(
-                nn.Conv2d(in_ch, out_ch, 3, padding=1, padding_mode='reflect'),
-                nn.BatchNorm2d(out_ch),
-                nn.ReLU(inplace=True)
-            )
-
-        def attention_block(in_ch):
-            return nn.Sequential(
-                nn.Conv2d(in_ch, in_ch, 1),
-                nn.Sigmoid()
-            )
-
-        # Энкодер (легкий, для мобильности)
-        self.enc1 = conv_block(in_channels, 32)
-        self.enc2 = conv_block(32, 64)
-        self.enc3 = conv_block(64, 128)
-        self.pool = nn.MaxPool2d(2, 2)
-
-        # Бутылочное горлышко с attention
-        self.bottleneck = conv_block(128, 256)
-        self.attention = attention_block(256)
-
-        # Декодер
-        self.upconv3 = nn.ConvTranspose2d(256, 128, 2, stride=2)
-        self.dec3 = conv_block(256, 128)
-        self.upconv2 = nn.ConvTranspose2d(128, 64, 2, stride=2)
-        self.dec2 = conv_block(128, 64)
-        self.upconv1 = nn.ConvTranspose2d(64, 32, 2, stride=2)
-        self.dec1 = conv_block(64, 32)
-        self.final = nn.Conv2d(32, out_channels, 1)
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
 
     def forward(self, x):
-        # Энкодер
+        return self.conv(x)
+
+
+class UNet(nn.Module):
+    def __init__(self, in_channels=1, out_channels=3):
+        super(UNet, self).__init__()
+
+        # Encoder
+        self.enc1 = DoubleConv(in_channels, 64)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.enc2 = DoubleConv(64, 128)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.enc3 = DoubleConv(128, 256)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Bottleneck
+        self.bottleneck = DoubleConv(256, 512)
+
+        # Decoder
+        self.upconv3 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.dec3 = DoubleConv(512, 256)
+        self.upconv2 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.dec2 = DoubleConv(256, 128)
+        self.upconv1 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.dec1 = DoubleConv(128, 64)
+
+        # Final layer
+        self.out = nn.Conv2d(64, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        # Encoder
         e1 = self.enc1(x)
-        e2 = self.enc2(self.pool(e1))
-        e3 = self.enc3(self.pool(e2))
-        b = self.bottleneck(self.pool(e3))
+        p1 = self.pool1(e1)
+        e2 = self.enc2(p1)
+        p2 = self.pool2(e2)
+        e3 = self.enc3(p2)
+        p3 = self.pool3(e3)
 
-        # Attention
-        attn = self.attention(b)
-        b = b * attn
+        # Bottleneck
+        b = self.bottleneck(p3)
 
-        # Декодер
+        # Decoder with skip connections
         d3 = self.upconv3(b)
-        d3 = torch.cat((d3, e3), dim=1)
+        d3 = torch.cat([d3, e3], dim=1)
         d3 = self.dec3(d3)
         d2 = self.upconv2(d3)
-        d2 = torch.cat((d2, e2), dim=1)
+        d2 = torch.cat([d2, e2], dim=1)
         d2 = self.dec2(d2)
         d1 = self.upconv1(d2)
-        d1 = torch.cat((d1, e1), dim=1)
+        d1 = torch.cat([d1, e1], dim=1)
         d1 = self.dec1(d1)
-        return torch.sigmoid(self.final(d1))
+
+        out = self.out(d1)
+        return torch.sigmoid(out)
+
+
+class TintoraAI(nn.Module):
+    def __init__(self):
+        super(TintoraAI, self).__init__()
+        self.unet = UNet()
+
+    def forward(self, x):
+        return self.unet(x)
