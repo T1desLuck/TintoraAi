e4d35b973d091e2d40253721ce4804adea5472d0
T1desLuck <tidesluck@icloud.com>
2025-07-22 11:00:01 +0500

    Update preprocess.py

--- src/model/preprocess.py
+++ src/model/preprocess.py
@@ -12,10 +12,10 @@ def preprocess_image(image, pad_divisor=8):
     pad_w = (pad_divisor - w % pad_divisor) % pad_divisor
     pad_h = (pad_divisor - h % pad_divisor) % pad_divisor
     padded_image = Image.new(
-        'L', 
-        (w + pad_w, h + pad_h), 
+        'L',
+        (w + pad_w, h + pad_h),
         0
-    )  # Добавление padding
+    ) # Добавление padding
     padded_image.paste(image, (0, 0))
     img_array = np.array(padded_image) / 255.0
     img_tensor = (torch.from_numpy(img_array)
