7d8fe3fb9a2765bb81af9c6ce446ef3543952d38
T1desLuck <tidesluck@icloud.com>
2025-07-22 12:21:20 +0500

    Update tintora_ai.py

--- src/model/tintora_ai.py
+++ src/model/tintora_ai.py
@@ -76,7 +76,8 @@ class ObjectClassifier(nn.Module):
     def __init__(self):
         super(ObjectClassifier, self).__init__()
         self.resnet = resnet18(pretrained=True)
-        self.resnet.fc = nn.Linear(self.resnet.fc.in_features, 10)  # 10 классов (лицо, одежда и т.д.)
+        self.resnet.fc = nn.Linear(
+            self.resnet.fc.in_features, 10)  # 10 классов (лицо, одежда и т.д.)
 
     def forward(self, x):
         return self.resnet(x)
