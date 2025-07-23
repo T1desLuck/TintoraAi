c8d8df018e4f0b1476887d0d83ac503dfedcc979
T1desLuck <tidesluck@icloud.com>
2025-07-23 16:48:31 +0500

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -6,7 +6,9 @@ def test_model_forward():
     model = TintoraAI()
     input_tensor = torch.randn(1, 1, 64, 64)  # Уменьшен до 64x64
     color_output, semantic_output = model(input_tensor)
-    assert color_output.shape == (1, 3, 64, 64), "Color output shape mismatch"
-    assert semantic_output.shape == (1, 10), "Semantic output shape mismatch"
+    assert color_output.shape == (1, 3, 64, 64), \
+           "Color output shape mismatch"
+    assert semantic_output.shape == (1, 99), \
+           "Semantic output shape mismatch"
     print(f"Color output shape: {color_output.shape}")
     print(f"Semantic output shape: {semantic_output.shape}")
