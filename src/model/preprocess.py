4784105e02beff059f5ed9e3a0b3b4ca87aeea83
T1desLuck <tidesluck@icloud.com>
2025-07-24 13:05:28 +0500

    Update preprocess.py

--- src/model/preprocess.py
+++ src/model/preprocess.py
@@ -3,19 +3,22 @@ import torch
 import numpy as np
 
 
-def preprocess_image(image, pad_divisor=8):
+def preprocess_image(image, pad_divisor=8, min_size=256):
     """Конвертирует изображение в ч/б и добавляет padding
     для совместимости с U-Net.
     """
-    image = image.convert("L")  # Чёрно-белое
+    try:
+        image = image.convert("L")  # Чёрно-белое
+    except Exception as e:
+        raise ValueError(f"Ошибка при конвертации изображения: {e}")
+
     w, h = image.size
+    if w < min_size or h < min_size:
+        raise ValueError(f"Изображение слишком маленькое. Минимальный размер {min_size}x{min_size} пикселей")
+
     pad_w = (pad_divisor - w % pad_divisor) % pad_divisor
     pad_h = (pad_divisor - h % pad_divisor) % pad_divisor
-    padded_image = Image.new(
-        'L',
-        (w + pad_w, h + pad_h),
-        0
-    )  # Добавление padding
+    padded_image = Image.new('L', (w + pad_w, h + pad_h), 0)
     padded_image.paste(image, (0, 0))
     img_array = np.array(padded_image) / 255.0
     img_tensor = (torch.from_numpy(img_array)
