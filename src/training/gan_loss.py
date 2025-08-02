6b1e3070f95d2a4b325c35b23b027017d336a6b3
T1desLuck <tidesluck@icloud.com>
2025-08-02 11:17:12 +0500

    Update gan_loss.py

--- src/training/gan_loss.py
+++ src/training/gan_loss.py
@@ -11,7 +11,7 @@ class GANLoss(nn.Module):
         super(GANLoss, self).__init__()
         self.register_buffer('real_label', torch.tensor(target_real_label))
         self.register_buffer('fake_label', torch.tensor(target_fake_label))
-        
+
         if use_lsgan:
             # Least squares GAN loss (стабильнее)
             self.loss = nn.MSELoss()
@@ -45,11 +45,11 @@ class PerceptualLoss(nn.Module):
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1)
         self.conv4 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1)
         self.relu = nn.ReLU(inplace=True)
-        
+
         # Инициализируем веса случайным образом, но фиксируем их для стабильности
         for param in self.parameters():
             param.requires_grad = False
-            
+
         # Слои нормализации для стабильности
         self.norm1 = nn.InstanceNorm2d(32)
         self.norm2 = nn.InstanceNorm2d(64)
@@ -63,28 +63,28 @@ class PerceptualLoss(nn.Module):
                 x = F.interpolate(x, size=y.shape[-2:], mode='bilinear', align_corners=True)
             else:
                 y = F.interpolate(y, size=x.shape[-2:], mode='bilinear', align_corners=True)
-        
+
         # Извлекаем признаки разных уровней
         x1 = self.relu(self.norm1(self.conv1(x)))
         y1 = self.relu(self.norm1(self.conv1(y)))
-        
+
         x2 = self.relu(self.norm2(self.conv2(x1)))
         y2 = self.relu(self.norm2(self.conv2(y1)))
-        
+
         x3 = self.relu(self.norm3(self.conv3(x2)))
         y3 = self.relu(self.norm3(self.conv3(y2)))
-        
+
         x4 = self.relu(self.norm4(self.conv4(x3)))
         y4 = self.relu(self.norm4(self.conv4(y3)))
-        
+
         # Вычисляем потери на разных уровнях
         loss1 = F.mse_loss(x1, y1)
         loss2 = F.mse_loss(x2, y2)
         loss3 = F.mse_loss(x3, y3)
         loss4 = F.mse_loss(x4, y4)
-        
+
         # Взвешенная сумма потерь
         # Придаем больший вес более высоким уровням для сохранения структурных элементов
         total_loss = 0.1 * loss1 + 0.2 * loss2 + 0.3 * loss3 + 0.4 * loss4
-        
+
         return total_loss
